e242a601e9ddd92baeb18178a56ab5bff647b516
Lasse Haffke <lasse@haffke.me>
2023-07-05 19:54:49 +0200

    feat: move all schemata to subdirectory

--- main.tf
+++ main.tf
@@ -34,14 +34,14 @@ resource "google_bigquery_table" "main" {
   deletion_protection = false
   dataset_id          = google_bigquery_dataset.raw_data.dataset_id
   table_id            = "raw__road_data"
-  schema              = file("schema_road_data.json")
+  schema              = file("schemata/schema_road_data.json")
 }
 
 resource "google_bigquery_table" "import" {
   deletion_protection = false
   dataset_id          = google_bigquery_dataset.raw_data.dataset_id
   table_id            = "raw_road_data_import"
-  schema              = file("schema_road_data.json")
+  schema              = file("schemata/schema_road_data.json")
 }
 
 resource "google_pubsub_subscription" "raw_data_ingest" {
@@ -96,23 +96,23 @@ resource "google_bigquery_dataset" "timestamp_road_mapping" {
 resource "google_bigquery_table" "timestamp_road_mapping" {
   dataset_id = "timestamp_road_mapping"
   table_id   = "timestamp_road_mapping"
-  schema     = file("schema_timestamp_mapping.json")
+  schema     = file("schemata/schema_timestamp_mapping.json")
 }
 
 resource "google_bigquery_table" "road_config" {
   dataset_id          = "road_config"
-  schema              = file("schema_road_config.json")
+  schema              = file("schemata/schema_road_config.json")
   table_id            = "road_config"
 }
 
 resource "google_bigquery_table" "road_config_geo" {
   dataset_id          = "road_config"
-  schema              = file("schema_road_config_geo.json")
+  schema              = file("schemata/schema_road_config_geo.json")
   table_id            = "road_config_geo"
 }
 
 resource "google_bigquery_table" "sensor_target" {
   dataset_id          = "road_config"
-  schema              = file("schema_sensor_target.json")
+  schema              = file("schemata/schema_sensor_target.json")
   table_id            = "sensor_target"
 }
