7fa69c9c76e4ea58322d1245d6bbb1a1eabb21b4
Abhinav Shrivastava <abhinav.shrivastava@forto.com>
2023-07-05 15:46:02 +0200

    Auto stash before merge of "main" and "origin/main"

--- green-hat-node/index.js
+++ green-hat-node/index.js
@@ -1,6 +1,32 @@
-var http = require('http');
+const express = require('express');
+const { BigQuery } = require('@google-cloud/bigquery');
+const app = express();
+const bigquery = new BigQuery();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080);
+app.get('/query_data', async (req, res) => {
+    const query = `
+        SELECT road_id, osm_fclass, AVG(no2_ppb) as avg_no2_ppb, AVG(co2_ppm) as avg_co2_ppm
+        FROM 'road_config.road_config'
+        WHERE osm_fclass = 'service'
+        GROUP BY road_id, osm_fclass
+        ORDER BY avg_no2_ppb DESC
+        LIMIT 10;
+    `;
+
+    const options = {
+        query: query,
+        location: 'US',
+    };
+
+    try {
+        const [rows] = await bigquery.query(options);
+        res.send(rows);
+    } catch (error) {
+        console.error('ERROR:', error);
+        res.status(500).send('Failed to query BigQuery');
+    }
+});
+
+app.listen(8000, () => {
+    console.log('Server running on port 8000');
+});
