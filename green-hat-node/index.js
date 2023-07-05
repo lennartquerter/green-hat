b2fe8a5b5a2a0fbea496c1d86de0a8e6754f59d8
Abhinav Shrivastava <abhinav.shrivastava@forto.com>
2023-07-05 16:37:00 +0200

    Add function to map to way

--- green-hat-node/index.js
+++ green-hat-node/index.js
@@ -1,31 +1,25 @@
+import fetch from 'node-fetch';
 const express = require('express');
 const { BigQuery } = require('@google-cloud/bigquery');
 const app = express();
 const bigquery = new BigQuery();
 
-app.get('/query_data', async (req, res) => {
-    const query = `
-        SELECT road_id, osm_fclass, AVG(no2_ppb) as avg_no2_ppb, AVG(co2_ppm) as avg_co2_ppm
-        FROM 'road_config.road_config'
-        WHERE osm_fclass = 'service'
-        GROUP BY road_id, osm_fclass
-        ORDER BY avg_no2_ppb DESC
-        LIMIT 10;
-    `;
+app.post('/map-to-osm-way', async (req, res) => {
+    console.log('Received request', req.body);
+});
 
-    const options = {
-        query: query,
-        location: 'US',
-    };
+async function fetchData(lat, lon) {
+    const url = `https://nominatim.openstreetmap.org/reverse.php?lat=${lat}&lon=${lon}&zoom=16&format=jsonv2`;
 
     try {
-        const [rows] = await bigquery.query(options);
-        res.send(rows);
+        const response = await fetch(url);
+        const body = await response.json();
+        return body.osm_id;
     } catch (error) {
-        console.error('ERROR:', error);
-        res.status(500).send('Failed to query BigQuery');
+        console.error(error);
+        return null;  // Return null or some error value when there's an exception
     }
-});
+}
 
 app.listen(8000, () => {
     console.log('Server running on port 8000');
