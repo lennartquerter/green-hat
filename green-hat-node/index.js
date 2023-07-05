9108b41dbfe33765b16fd95a0a43bae70a559e0e
Abhinav Shrivastava <abhinav.shrivastava@forto.com>
2023-07-05 16:49:02 +0200

    Fix build

--- green-hat-node/index.js
+++ green-hat-node/index.js
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch';
 const express = require('express');
 const { BigQuery } = require('@google-cloud/bigquery');
 const app = express();
@@ -8,19 +7,19 @@ app.post('/map-to-osm-way', async (req, res) => {
     console.log('Received request', req.body);
 });
 
-async function fetchData(lat, lon) {
-    const url = `https://nominatim.openstreetmap.org/reverse.php?lat=${lat}&lon=${lon}&zoom=16&format=jsonv2`;
+// async function fetchData(lat, lon) {
+//     const url = `https://nominatim.openstreetmap.org/reverse.php?lat=${lat}&lon=${lon}&zoom=16&format=jsonv2`;
+//
+//     try {
+//         const response = await fetch(url);
+//         const body = await response.json();
+//         return body.osm_id;
+//     } catch (error) {
+//         console.error(error);
+//         return null;  // Return null or some error value when there's an exception
+//     }
+// }
 
-    try {
-        const response = await fetch(url);
-        const body = await response.json();
-        return body.osm_id;
-    } catch (error) {
-        console.error(error);
-        return null;  // Return null or some error value when there's an exception
-    }
-}
-
-app.listen(8000, () => {
-    console.log('Server running on port 8000');
+app.listen(8080, () => {
+    console.log('Server running on port 8080');
 });
